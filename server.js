7986ba861b2adbaaae72ed050f30b15522f7b9e2
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-08-02 17:34:22 +0530

    netlify

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(
   cors({
-    origin: "https://clever-pudding-31c61d.netlify.app",
+    origin: "https://startling-figolla-c506c8.netlify.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
